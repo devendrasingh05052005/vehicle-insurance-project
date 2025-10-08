5b6edb5d37640db3fe4935b2b71c8656d699a664
devendra <banad8640@gmail.com>
2025-10-08 13:15:07 +0530

    exception added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Devendra Singh",
+    author_email="banad8640@gmail.com",
+    packages=find_packages()
+)
